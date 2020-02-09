a025f889adb005b88140148a7934f767aac4a7b2
Shibayama <k.shibayama0730@gmail.com>
2020-02-09 12:41:45 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by ShibayamaKentaro on 2020/02/09.
+//  Copyright © 2020 KKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
